e1229b248345964ae81178c9d4263b354f728881
Rodriguesdobem <rm96930@estudante.fieb.edu.br>
2025-06-10 10:43:45 +0000

    Flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home.jsx'
+import Menu from './Menu'
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
+
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="home" element={<Home/>} />
+      <Route index element={<Menu/>} />
+      <Route path="pokeflex" element={<PokeFlex/>} />
+      <Route path="pokegrid" element={<PokeGrid/>} />
     </Routes>
   )
 }
---
+++ src/Menu.jsx
@@ -0,0 +1,22 @@
+const Menu = () => {
+
+    return (
+     <div className = "Menu">
+          <nav>
+            <ul>
+                <li>
+     <a href="/home">Home</a>
+              </li> 
+              <li>
+     <a href="/pokeflex">Flex</a>    
+              </li>
+              <li>
+     <a href="/pokegrid">Grid</a>       
+              </li>
+              </ul>
+         </nav>  
+      </div>
+    )
+}
+
+export default Menu
